6593b44058769bf57cbe0ed9fff41f6280be6fd4
Santiago836-png <santi.mprm@gmail.com>
2025-06-05 15:29:53 -0500

    Creacion solucion proyecto

---
+++ ConceptosProgramación/numeroparimpar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+do
+{
+    Console.Write("ingrese un numero:");
+    var numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("el numero es par");
+    }
+    else
+    {
+        Console.WriteLine("el numero es impar");
+    }
+
+
+}while (true);
+
+
+
+
+
+
+
+
+
+
+
+
